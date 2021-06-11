c40dcc05a4cd788f725d5ff2868a363d06ad9c65
LuisJackRamirez <l_armando_2000@hotmail.com>
2021-06-11 15:36:05 -0500

    Script now fetches the first thumbnail as image

--- API/app/writeToDB.py
+++ API/app/writeToDB.py
@@ -2,11 +2,9 @@ import json
 import os
 import pandas as pds
 import re
+import requests
 import sqlite3
 import sys
-from selenium.webdriver.chrome.options import Options
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 from getLatLng import getAddress
 
@@ -112,11 +110,23 @@ def writePageToDB (propiedadesInfo):
 
     # Arreglo que almacena la información de todas las tarjetas de propiedad
     cardsList = []
+    imgDict = {}
 
     for i in cards:
         # Ubicar la tarjeta de propiedad y almacenarla como objeto
         card = i.find (class_='row m-0').find (class_='col-sm-8 pt-2 px-2 pb-0')
         cardInfo = getCardInfo (card)
+
+        imgUrl = i.find (class_='image-gallery-image')['src']
+        try:
+            img = requests.get (imgUrl).content
+            if not img[:4] == b'\xff\xd8\xff\xe0':
+                raise Exception ("File not supported")
+        except Exception as e:
+            print (imgUrl, ": Error retrieiving thumbnail --", e)
+            img = None
+        finally:
+            imgDict[cardInfo["link"]] = img
 
         cardPriceInfo = i.find (class_='card-footer p-0 border-0 bg-transparent').find ('h5').text
         cardInfo["price"] = float (parsePrice (cardPriceInfo))
@@ -144,7 +154,7 @@ def writePageToDB (propiedadesInfo):
         # cursor.execute (""" DROP TABLE IF EXISTS INMUEBLE; """)    
         # cursor.execute ( """ DELETE FROM USUARIOS WHERE Correo="century21.realestate@century21.com"; """)
 
-        try:
+        '''try:
             with open ('c21.jpg', 'rb') as file:
                 binaryData = file.read ()
 
@@ -154,7 +164,7 @@ def writePageToDB (propiedadesInfo):
             conn.commit ()
         except sqlite3.Error as error1:
             #print("Failed inserting data into USUARIOS table: {}".format(error1))
-            raise Exception("Failed inserting data into USUARIOS table: {}".format(error1))
+            raise Exception("Failed inserting data into USUARIOS table: {}".format(error1))'''
 
         # primary key (Propietario, Id),
         # [Id] integer primary key,
@@ -197,8 +207,8 @@ def writePageToDB (propiedadesInfo):
                 )''')
 
         query = """ INSERT OR IGNORE INTO INMUEBLE (Propietario, Nombre, Direccion, Latitud, Longitud, Terreno, Construccion, NumHabitacion, NumSanitario,
-                                            NumEstacionamiento, Descripcion, Precio) 
-                    VALUES (?,?,?,?,?,?,?,?,?,?,?,?) """
+                                            NumEstacionamiento, Descripcion, Precio, Imagen) 
+                    VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?) """
 
         # l[0] = Nombre
         # l[1] = Direccion
@@ -210,6 +220,7 @@ def writePageToDB (propiedadesInfo):
         # l[7] = Estacionamiento
         # l[8] = Descripcion
         # l[9] = Precio
+        # l[10] = Imagen
         for row in data.index:
             l = []
             for col in data.columns:
@@ -218,7 +229,7 @@ def writePageToDB (propiedadesInfo):
             address = getAddress (l[2])
 
             if address != None:
-                cursor.execute (query, ("century21.realestate@century21.com", l[0], address[0], address[1][0], address[1][1], int(l[3]), int(l[4]), int(l[5]), int(l[6]), int(l[7]), l[8], l[9]))
+                cursor.execute (query, ("century21.realestate@century21.com", l[0], address[0], address[1][0], address[1][1], int(l[3]), int(l[4]), int(l[5]), int(l[6]), int(l[7]), l[8], l[9], imgDict[l[2]]))
                 conn.commit ()
 
         # cursor.execute ( """ DELETE FROM INMUEBLE WHERE Id is NULL; """)
