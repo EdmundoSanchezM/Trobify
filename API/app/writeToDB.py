f730b3be23db7f39d578618db932041c6fe625ba
LuisJackRamirez <l_armando_2000@hotmail.com>
2021-06-11 12:21:25 -0500

    Script now fetches price, added CITAS table

--- API/app/writeToDB.py
+++ API/app/writeToDB.py
@@ -4,6 +4,9 @@ import pandas as pds
 import re
 import sqlite3
 import sys
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 from getLatLng import getAddress
 
@@ -51,6 +54,14 @@ def parseToString (propertyInfo, it):
 
     string = string[:j]
     return string
+
+def parsePrice (price):
+    res = str ()
+    for i in price:
+        if i.isnumeric ():
+            res = res + i
+
+    return res
 
 def getCardInfo (card):
     # Diccionario para guardar información de una propiedad
@@ -106,6 +117,9 @@ def writePageToDB (propiedadesInfo):
         # Ubicar la tarjeta de propiedad y almacenarla como objeto
         card = i.find (class_='row m-0').find (class_='col-sm-8 pt-2 px-2 pb-0')
         cardInfo = getCardInfo (card)
+
+        cardPriceInfo = i.find (class_='card-footer p-0 border-0 bg-transparent').find ('h5').text
+        cardInfo["price"] = float (parsePrice (cardPriceInfo))
         cardsList.append (cardInfo)
 
     # Con el diccionario listo, se crea un archivo JSON
@@ -114,6 +128,7 @@ def writePageToDB (propiedadesInfo):
     # Lectura de JSON
     try: 
         data = pds.read_json ("cardList.json").convert_dtypes ()
+        data["price"] = data["price"].astype (float)
         print ("JSON successfully opened")
     except:
         raise Exception("Couldn't open JSON")
@@ -156,6 +171,8 @@ def writePageToDB (propiedadesInfo):
                 [NumSanitario] integer not null,
                 [NumEstacionamiento] integer not null,
                 [Descripcion] text not null,
+                [Precio] real default 0,
+                [Imagen] blob default null,
                 
                 unique (Propietario, Latitud, Longitud),
                 constraint fk_Propietario
@@ -165,9 +182,23 @@ def writePageToDB (propiedadesInfo):
                     ON DELETE CASCADE
                 )''')
 
+        cursor.execute('''CREATE TABLE IF NOT EXISTS CITAS (
+                [Solicitante] text not null,
+                [Latitud] real not null,
+                [Longitud] real not null,
+                [Fecha_hora] text not null,
+                
+                unique (Solicitante, Latitud, Longitud),
+                constraint fk_Solicitante
+                    FOREIGN KEY (Solicitante)
+                    REFERENCES USUARIOS (Correo)
+                    ON UPDATE CASCADE
+                    ON DELETE CASCADE
+                )''')
+
         query = """ INSERT OR IGNORE INTO INMUEBLE (Propietario, Nombre, Direccion, Latitud, Longitud, Terreno, Construccion, NumHabitacion, NumSanitario,
-                                            NumEstacionamiento, Descripcion) 
-                    VALUES (?,?,?,?,?,?,?,?,?,?,?) """
+                                            NumEstacionamiento, Descripcion, Precio) 
+                    VALUES (?,?,?,?,?,?,?,?,?,?,?,?) """
 
         # l[0] = Nombre
         # l[1] = Direccion
@@ -178,6 +209,7 @@ def writePageToDB (propiedadesInfo):
         # l[6] = Sanitarios
         # l[7] = Estacionamiento
         # l[8] = Descripcion
+        # l[9] = Precio
         for row in data.index:
             l = []
             for col in data.columns:
@@ -186,7 +218,7 @@ def writePageToDB (propiedadesInfo):
             address = getAddress (l[2])
 
             if address != None:
-                cursor.execute (query, ("century21.realestate@century21.com", l[0], address[0], address[1][0], address[1][1], int(l[3]), int(l[4]), int(l[5]), int(l[6]), int(l[7]), l[8]))
+                cursor.execute (query, ("century21.realestate@century21.com", l[0], address[0], address[1][0], address[1][1], int(l[3]), int(l[4]), int(l[5]), int(l[6]), int(l[7]), l[8], l[9]))
                 conn.commit ()
 
         # cursor.execute ( """ DELETE FROM INMUEBLE WHERE Id is NULL; """)
